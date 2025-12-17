e6f9bfe928b4ecb6dd5164438581d4d512fbabf6
Groove Salad <95263024+Priscillalala@users.noreply.github.com>
2025-12-17 14:47:40 -0800

    add continue teleporter music

---
+++ plugins/Extras/ContinueTeleporterMusic.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx.Configuration;
+using RoR2;
+using RoR2.WwiseUtils;
+using UnityEngine;
+
+namespace MusicSwapper.Extras;
+
+public static class ContinueTeleporterMusic
+{
+    public static void Init()
+    {
+        ConfigEntry<bool> configEntry = MusicSwapperPlugin.ExtrasConfig.Bind(
+            "Teleporter Music",
+            "Continue After Boss",
+            false,
+            "Continue playing the Teleporter track until you leave the stage.");
+        if (configEntry.Value)
+        {
+            MusicSwapperPlugin.Logger.LogMessage($"{nameof(ContinueTeleporterMusic)} extra is active");
+            On.RoR2.MusicController.UpdateTeleporterParameters += MusicController_UpdateTeleporterParameters;
+        }
+    }
+
+    private static void MusicController_UpdateTeleporterParameters(On.RoR2.MusicController.orig_UpdateTeleporterParameters orig, MusicController self, TeleporterInteraction teleporter, Transform cameraTransform, CharacterBody targetBody)
+    {
+        orig(self, teleporter, cameraTransform, targetBody);
+        bool stopBossMusic = (teleporter && teleporter.isInFinalSequence) || (Run.instance && Run.instance.inSceneTransition);
+        self.stBossStatus.valueId = stopBossMusic ? CommonWwiseIds.dead : CommonWwiseIds.alive;
+    }
+}
